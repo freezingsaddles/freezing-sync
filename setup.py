26d3c0f7e44b5958c835b1d04bdb318200d7536d
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2019-01-01 00:53:52 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:  # for pip <= 9.0.3
 
 from setuptools import setup
 
-version = '1.1.0'
+version = '1.1.1'
 
 long_description = """
 freezing-sync is the component responsible for fetching activities, weather data, etc.
