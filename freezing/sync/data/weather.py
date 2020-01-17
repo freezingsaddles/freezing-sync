c9364dba0b631421c7cc7b900f2f5dec74494043
merlin <merlin@merlin.org>
2020-01-16 19:37:33 -0500

    hand rolled dark sky

--- freezing/sync/data/weather.py
+++ freezing/sync/data/weather.py
@@ -1,14 +1,13 @@
 import logging
 from datetime import timedelta
 from decimal import Decimal
+from statistics import mean
 
 from sqlalchemy import text
-from pytz import timezone
 
 from freezing.model import meta, orm
 
 from freezing.sync.utils.wktutils import parse_point_wkt
-from freezing.sync.wx.sunrise import Sun
 from freezing.sync.wx.darksky.api import HistoDarkSky
 
 from freezing.sync.config import config
@@ -48,7 +47,7 @@ class WeatherSync(BaseSync):
             ;
             """)
 
-        darksky = HistoDarkSky(
+        dark_sky = HistoDarkSky(
             api_key=config.DARK_SKY_API_KEY,
             cache_dir=config.DARK_SKY_CACHE_DIR,
             cache_only=cache_only,
@@ -79,57 +78,51 @@ class WeatherSync(BaseSync):
 
                 self.logger.debug("Ride metadata: time={0} dur={1} loc={2}/{3}".format(ride.start_date, ride.elapsed_time, lat, lon))
 
-                hist = darksky.histo_forecast(time=ride.start_date, latitude=lat, longitude=lon)
+                hist = dark_sky.histo_forecast(time=ride.start_date, latitude=lat, longitude=lon)
 
                 self.logger.debug("Got response in timezone {0}".format(hist.timezone))
 
-                ride_start = ride.start_date.replace(tzinfo=timezone(hist.timezone))
+                ride_start = ride.start_date.replace(tzinfo = hist.timezone)
                 ride_end = ride_start + timedelta(seconds=ride.elapsed_time)
 
                 # NOTE: if elapsed_time is significantly more than moving_time then we need to assume
                 # that the rider wasn't actually riding for this entire time (and maybe just grab temps closest to start of
                 # ride as opposed to averaging observations during ride.
 
-                ride_observations = [d for d in hist.hourly.data if ride_start <= d.time <= ride_end]
+                ride_observations = [d for d in hist.hourly if ride_start <= d.time <= ride_end]
 
-                start_obs = min(hist.hourly.data, key=lambda d:abs((d.time - ride_start).total_seconds()))
-                end_obs = min(hist.hourly.data, key=lambda d:abs((d.time - ride_end).total_seconds()))
-                daily = hist.daily.data[0]
+                start_obs = min(hist.hourly, key=lambda d:abs((d.time - ride_start).total_seconds()))
+                end_obs = min(hist.hourly, key=lambda d:abs((d.time - ride_end).total_seconds()))
 
-                def avg(l):
-                    no_nulls = [e for e in l if e is not None]
-                    if not no_nulls:
-                        return None
-                    return sum(no_nulls) / len(no_nulls) * 1.0  # to force float
+                if len(ride_observations) <= 2:
+                    # if we don't have many observations, bookend the list with the start/end observations without double counting
+                    ride_observations = [start_obs] + [o for o in ride_observations if o is not start_obs and o is not end_obs] + [end_obs]
+
+                for x in ride_observations:
+                    self.logger.debug("Observation: {0}".format(x.__dict__))
 
                 rw = orm.RideWeather()
                 rw.ride_id = ride.id
                 rw.ride_temp_start = start_obs.temperature
                 rw.ride_temp_end = end_obs.temperature
-                if len(ride_observations) <= 2:
-                    # if we don't have many observations, bookend the list with the start/end observations without double counting
-                    ride_observations = [start_obs] + [o for o in ride_observations if o is not start_obs and o is not end_obs] + [end_obs]
 
-                rw.ride_temp_avg = avg([o.temperature for o in ride_observations])
+                rw.ride_temp_avg = mean([o.temperature for o in ride_observations])
 
                 rw.ride_windchill_start = start_obs.apparent_temperature
                 rw.ride_windchill_end = end_obs.apparent_temperature
-                rw.ride_windchill_avg = avg([o.apparent_temperature for o in ride_observations])
+                rw.ride_windchill_avg = mean([o.apparent_temperature for o in ride_observations])
 
-                for x in ride_observations:
-                    self.logger.debug("Observation: {0}".format(x.__dict__))
+                # scale the cumulative precipitation over the observation period by the fraction of time spent moving
+                scale = timedelta(hours=len(ride_observations)).total_seconds() / ride.moving_time
+                rw.ride_precip = sum([o.precip_accumulation for o in ride_observations]) / scale
+                rw.ride_rain = any([o.precip_type == 'rain' for o in ride_observations])
+                rw.ride_snow = any([o.precip_type == 'snow' for o in ride_observations])
 
-                # Sometimes attributes are None, sometimes they are AttributeError
-                precip_type = lambda h: getattr(h, 'precip_type', None)
-                precip_intensity = lambda h: getattr(h, 'precip_intensity', None) or 0
+                rw.day_temp_min = hist.daily.temperature_min
+                rw.day_temp_max = hist.daily.temperature_max
 
-                # precipitation is a bit wonky. intensity is mm/hour, accumulation is never set. but it's something.
-                rw.ride_precip = sum([precip_intensity(o) for o in ride_observations])
-                rw.ride_rain = any([precip_type(o) == 'rain' for o in ride_observations])
-                rw.ride_snow = any([precip_type(o) == 'snow' for o in ride_observations])
-
-                rw.day_temp_min = daily.temperature_min
-                rw.day_temp_max = daily.temperature_max
+                rw.sunrise = hist.daily.sunrise_time.time()
+                rw.sunset = hist.daily.sunset_time.time()
 
                 self.logger.debug("Ride weather: {0}".format(rw.__dict__))
 
@@ -139,15 +132,6 @@ class WeatherSync(BaseSync):
 
                 sess.add(rw)
                 sess.flush()
-
-                if lat and lon:
-                    try:
-                        sun = Sun(lat=lat, lon=lon)
-                        rw.sunrise = sun.sunrise(ride_start)
-                        rw.sunset = sun.sunset(ride_start)
-                    except:
-                        self.logger.exception("Error getting sunrise/sunset for ride {0}".format(ride))
-                        # But soldier on ...
 
             except:
                 self.logger.exception("Error getting weather data for ride: {0}".format(ride))
--- freezing/sync/wx/darksky/api.py
+++ freezing/sync/wx/darksky/api.py
@@ -5,8 +5,15 @@ from logging import Logger, getLogger
 
 from pickle import dump, load, UnpicklingError
 
-from darksky.api import DarkSky
-from darksky.types import weather
+from json import loads
+from requests import get
+from requests.exceptions import HTTPError
+
+from .model import Forecast
+
+
+# there is a nice dark sky library, but it is GPL and thus incompatible.
+# there are also not-so-nice dark sky libraries.
 
 
 class HistoDarkSky(object):
@@ -15,25 +22,38 @@ class HistoDarkSky(object):
     """
 
     def __init__(self, api_key: str, cache_dir: str = None, cache_only: bool = False, logger: Logger = None):
-        self.logger = logger or getLogger(__name__)
+        self.api_key = api_key
         self.cache_dir = cache_dir
         self.cache_only = cache_only
-        self.dark_sky = DarkSky(api_key)
+        self.logger = logger or getLogger(__name__)
         if cache_only and not cache_dir:
             raise RuntimeError('Cache only but no cache dir 8(')
 
-    def histo_forecast(self, time: datetime, longitude: float, latitude: float):
+    def histo_forecast(self, time: datetime, latitude: float, longitude: float) -> Forecast:
         return self._get_cached(
             time=time,
             longitude=longitude,
             latitude=latitude,
-            fetch=lambda: self.dark_sky.get_time_machine_forecast(
+            fetch=lambda: self.forecast(
                 time=time,
-                longitude=longitude,
                 latitude=latitude,
-                exclude=[weather.MINUTELY, weather.ALERTS]
+                longitude=longitude
             )
         )
+
+    def forecast(self, time: datetime, latitude: float, longitude: float) -> Forecast:
+        response = get(
+            url=f'https://api.darksky.net/forecast/{self.api_key}/{latitude},{longitude},{time.isoformat()}',
+            params={'units': 'us', 'exclude': 'minutely,alerts,flags'},
+            headers={'Accept-Encoding': 'gzip'},
+            timeout=15
+        )
+        if response.status_code is not 200:
+            raise HTTPError(
+                f'Bad response: {response.status_code} {response.reason}: {response.text}')
+        json = loads(response.text)
+        # self.logger.info(f'Blob: {json}')
+        return Forecast(json)
 
     def _get_cached(self, time: datetime, longitude: float, latitude: float, fetch):
         if not self.cache_dir:
@@ -41,8 +61,6 @@ class HistoDarkSky(object):
 
         directory = os.path.join(self.cache_dir, f'{longitude}x{latitude}')
         path = os.path.join(directory, f'{time.strftime("%Y-%m-%d")}.pykl')
-        if not os.path.exists(directory):
-            os.makedirs(directory)
 
         if os.path.exists(path):
             self.logger.debug(f'Cache hit for {path}')
@@ -57,8 +75,12 @@ class HistoDarkSky(object):
             raise RuntimeError(f'No cache entry for {path} and cache_only is true')
 
         self.logger.debug(f'Cache miss for {path}')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
         data = fetch()
         with open(path, 'wb') as file:
             dump(data, file)
+
         return data
 
